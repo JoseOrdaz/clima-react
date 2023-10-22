46349d97d569cdc2fcda1807389a5c260acf6019
JoseOrdaz <joseordazsuay@gmail.com>
2023-10-22 21:38:11 +0200

    añadir resultado de la llamada a la api

--- componentes/Resultado.jsx
+++ componentes/Resultado.jsx
@@ -4,13 +4,18 @@ const Resultado = () => {
 
         const { resultado } = useClima()
 
-        const { name } = resultado
+        const { name, main } = resultado
 
-        console.log(resultado)
+        const kelvin = 273.15
 
   return (
     <div className="contenedor">
         <h2>El tiempo de {name} es: </h2>
+        <p>Temperatura actual: {parseInt(main.temp - kelvin)} <span>&#x2103;</span> </p>
+        <p>Temperatura maxima: {parseInt(main.temp_max - kelvin)} <span>&#x2103;</span> </p>
+        <p>Temperatura minima: {parseInt(main.temp_min - kelvin)} <span>&#x2103;</span> </p>
+
+
     </div>
   )
 }
